f39f4439872d4162fa99a3a5a40d297690864542
Niina <t7hani01@students.oamk.fi>
2019-10-14 17:35:08 +0300

    unohtui tämä
    tämä unohtui

--- mean-ruokalistasovellus/src/app/app.module.ts
+++ mean-ruokalistasovellus/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodDetailsComponent } from './foods/food-details/food-details.component';
+import { FoodListComponent } from './foods/food-list/food-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodDetailsComponent,
+    FoodListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
