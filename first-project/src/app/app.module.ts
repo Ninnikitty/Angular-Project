7b3a4a621ccb1b05b6fdf891fc7062df467cedfc
Niina <t7hani01@students.oamk.fi>
2020-01-27 15:23:18 +0200

    Added the bones a bit

--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import {MatListModule} from '@angular/material/list'; 
+import { MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,15 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule, 
+    MatSortModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
